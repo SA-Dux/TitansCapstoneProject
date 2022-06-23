995368449e9e9474dd1f5273b207e4f57b0762ce
Nurlan M <huplah.m@gmail.com>
2022-06-23 12:59:28 -0400

    Changes in class

--- src/day/one/PracticeClass.java
+++ src/day/one/PracticeClass.java
@@ -5,7 +5,7 @@ public class PracticeClass {
 	public static int main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
+		System.out.println("this is first line of code");
 		public int mult(int a, int b) {
 			int mult = a*b;
 			return mult;
